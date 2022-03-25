00897112209415910140e15d1855d9b4a3f1d050
Amol Ghotale <ghotaleamol@gmail.com>
2022-03-25 20:37:06 +0530

    ADDED and Showed: New contact details

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,8 +1,30 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to address book program");
+
+        int choice;
+        AddressBook ab = new AddressBook();
+        do {
+
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("Welcome to Address Book Program");
+            System.out.println("Kindly Enter choice \n 1. Add new Contact \n 2. Show Contact details");
+            choice = scan.nextInt();
+            switch (choice) {
+                case 1:
+                    ab.addContacts();
+                    break;
+                case 2:
+                    ab.showContacts();
+                case 3:
+                    ab.showContacts();
+                    break;
+            }
+        }while (choice != 5);
     }
 }
